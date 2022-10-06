65174cb65cb970ed68cf978ce6dc6db5cc62fca5
greenwhale00 <goreakay@gmail.com>
2022-10-06 17:28:47 +0900

    221006

---
+++ src/components/BigCate.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import '../css/Content.scss'
+
+const BigCate = () => {
+    return (
+        <div className='BigCate'>
+            <figure>
+
+            </figure>
+        </div>
+    )
+}
+
+export default BigCate
