2bc616e7f122e589b1e3845e15415ae5f7b90b9a
greenwhale00 <goreakay@gmail.com>
2022-10-06 15:52:33 +0530

    221006

--- src/components/Product.js
+++ src/components/Product.js
@@ -2,9 +2,16 @@ import React from 'react'
 
 const Product = () => {
   return (
-    <div className='Product'>
-        
-    </div>
+    <section className='Product'>
+<div className='row01'>
+    <figure>
+
+    </figure>
+    <figure>
+    <img src={process.env.PUBLIC_URL + "/assets/images/t_women.jpg"} alt="" />
+    </figure>
+</div>
+    </section>
   )
 }
 
---
+++ src/data/ProductData.js
@@ -0,0 +1,17 @@
+const product = [
+    { id: 1, title: "IN THE STAR", des:"IS_221408 Platform Middle Boots (3Colors)", price:"259200", link: "/" },
+    { id: 2, title: "JOSEPHJOSEPH", des:"이지 감자매셔(으깨기)", price:"11900", link: "/" },
+    { id: 3, title: "DROPDROPDROP", des:"행잉프린트커튼 COCO-A FLOWER", price:"94000", link: "/" },
+    { id: 4, title: "DROPDROPDROP", des:"행잉프린트커튼 CIMMAMONY FALL", price:"94000", link: "/" },
+    { id: 5, title: "DROPDROPDROP", des:"클리어쉬폰커튼 STARRY FLOWERY NIGHT", price:"84000", link: "/" },
+    { id: 6, title: "DROPDROPDROP", des:"쿠션커버 COCO-A FLOWER 50x50", price:"38000", link: "/" },
+    { id: 7, title: "DROPDROPDROP", des:"쿠션커버 CIMMAMONY FALL 50x50", price:"38000", link: "/" },
+    { id: 8, title: "CHAMPION", des:"[ASIA] Reverse Weave® (블루택) 11.5oz 기모 스웨트셔츠 RLX FIT (GREY) CKTS2F322G2", price:"179000", link: "/" },
+    { id: 9, title: "CHAMPION", des:"[ASIA] Reverse Weave® (블루택) 11.5oz 기모 스웨트셔츠 RLX FIT (BLACK) CKTS2F322BK", price:"179000", link: "/" },
+    { id: 10, title: "CHAMPION", des:"[ASIA] Reverse Weave® (블루택) 11.5oz 기모 스웨트셔츠 RLX FIT (NAVY) CKTS2F322N2", price:"179000", link: "/" },
+]
+
+
+
+
+export {product}
